ea3f0ec096157cb9b667d4cccf5691eaa1c79345
Victor Kiss <victorkiss10@gmail.com>
2021-02-05 23:13:31 -0300

    arquivos adicionados

---
+++ pages/index.jsx
@@ -0,0 +1,35 @@
+import NextHead from '../components/Head'
+import Footer from '../components/Footer'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faRocket} from '@fortawesome/free-solid-svg-icons'
+
+import Div from '../paterns/Div'
+import GridDiv from '../paterns/GridDiv'
+import FlexDiv from '../paterns/FlexDiv'
+
+import PhotoComponent from '../data/PhotoComponent'
+
+
+export default function Home() {
+  return (
+    <>
+    <NextHead title="Foto do dia"/>
+
+    <Div>
+       <GridDiv>
+        <FlexDiv justify="center" direction="column" align="center" padding="20px 0" height="100%" border="2px solid black" bgColor="#fbfbfb">
+          <h1>PICTURE OF DAY</h1>
+          <h2>
+              Using <a href="https://api.nasa.gov/#browseAPI" target="_blank" rel="noopener">API APOD</a>
+          </h2>
+          <FontAwesomeIcon icon={faRocket}/>
+      </FlexDiv>
+        <Div margin="10px" padding="10px">
+            <PhotoComponent/>
+        </Div>
+        </GridDiv>
+      <Footer/>
+    </Div>
+    </>
+  )
+}
